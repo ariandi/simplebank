362d1125ec5785d7c10d3661550e263bf71720bd
Ariandi Nugraha <db_duabelas@yahoo.com>
2022-03-13 20:41:42 +0700

    adding env file

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"database/sql"
+	"github.com/ariandi/simplebank/api"
+	db "github.com/ariandi/simplebank/db/sqlc"
+	"github.com/ariandi/simplebank/util"
+	_ "github.com/lib/pq"
+	"log"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Cannot load config : ", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("cannot connect to server", err)
+	}
+}
